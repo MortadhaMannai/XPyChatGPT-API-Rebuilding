f52be3ea197e46fdbc7cbc57cb793b8a521a2a99
MortadhaMannai <mannaimortadha898@gmail.com>
2023-12-30 18:43:21 +0100

    init

---
+++ chat copy.py
@@ -0,0 +1,45 @@
+import openai
+openai.api_key = "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" # Yoo NMTC PPS ! Replace this with your API key
+print("ChatGPT: Hi, I'm ChatGPT. I'm a helpful assistant")
+messages = [
+    # system message to set the behavior of the assistant
+    {"role": "system", "content": "Hi ChatGPT, You are a helpful assistant!"},
+]
+try:
+    chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+    reply = chat_completion["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": reply})
+except:
+    print("Check if you have set the API key correctly")
+    exit()
+
+while True:
+    message = input("👤: ")
+    # message = input("📄: ")
+    if message == "exit":
+        break
+    if message == "clear":
+        print("\033[H\033[J")
+        print("ChatGPT: Hi, I'm ChatGPT. I'm a helpful assistant")
+        messages = [
+            {"role": "system", "content": "Hi ChatGPT, You are a helpful assistant!"},
+        ]
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat_completion["choices"][0]["message"]["content"]
+        messages.append({"role": "assistant", "content": reply})
+        continue
+        
+    
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat_completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    
+    reply = chat_completion["choices"][0]["message"]["content"]
+    print(f"🤖: {reply}")
+    messages.append({"role": "assistant", "content": reply})
